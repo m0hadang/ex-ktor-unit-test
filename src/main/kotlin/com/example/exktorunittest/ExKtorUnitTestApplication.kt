6ae1dd01cbf11bc13d5ab5a5e054215f473dc96f
mohadang <ehdlaos@gmail.com>
2025-01-28 15:43:57 +0900

    init

---
+++ src/main/kotlin/com/example/exktorunittest/ExKtorUnitTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.exktorunittest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExKtorUnitTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExKtorUnitTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/exktorunittest/ExKtorUnitTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.exktorunittest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExKtorUnitTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
